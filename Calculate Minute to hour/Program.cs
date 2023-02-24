d0a6d12312145ccd8a543aa2e9b8440f5d674c90
admin <admin@CHAOS>
2023-02-25 00:53:19 +0700

    Add project files.

---
+++ Calculate Minute to hour/Program.cs
@@ -0,0 +1,6 @@
+﻿Console.WriteLine("Enter minute = ");
+int amn = int.Parse(Console.ReadLine());
+
+int amh = amn / 60;
+int rmn = amn % 60;
+Console.WriteLine("The amount you enter equal to {0} hour {1} minute", amh, rmn);
